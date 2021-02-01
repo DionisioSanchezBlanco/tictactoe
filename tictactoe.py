d44df2d8a0303746a28defbf4f4b9a580d7d0b48
Dioni <dsanbla@gmail.com>
2021-02-01 19:56:28 +0100

    Second stage

--- tictactoe.py
+++ tictactoe.py
@@ -4,5 +4,5 @@ xo_list = list(xo_string)
 
 print("---------")
 for _i in range(0, 3):
-    print(f"| {xo_list[_i * 3]} {xo_list[_i * 3 + 1]} {xo_list[_i * 3 + 2]}|")
+    print(f"| {xo_list[_i * 3]} {xo_list[_i * 3 + 1]} {xo_list[_i * 3 + 2]} |")
 print("---------")
