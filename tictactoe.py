d9ff9887cf2c78de7c61ecf95465b18863c766a8
Dioni <dsanbla@gmail.com>
2021-02-01 19:53:45 +0100

    Second stage

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,8 @@
 # Tic-tac-toe game
-print("""X O X
-O X O
-X X O""")
+xo_string = input("Enter cells:")
+xo_list = list(xo_string)
+
+print("---------")
+for _i in range(0, 3):
+    print(f"| {xo_list[_i * 3]} {xo_list[_i * 3 + 1]} {xo_list[_i * 3 + 2]}|")
+print("---------")
